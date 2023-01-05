c8629434631694a54e9d64e07fe56cea8e1e4f41
Igor Fernandes <96093472+igorcfer@users.noreply.github.com>
2023-01-05 12:36:19 -0500

    final project

---
+++ index.js
@@ -0,0 +1,6 @@
+const btnElement = document.getElementById("btn")
+const emojiNameElement = document.getElementById("emoji-name")
+
+btnElement.addEventListener("click", ()=>{
+    console.log("cliked");
+})
