b15b3a1e34f4bb9623c316fe7cbb1dbd7a96e79b
Igor Fernandes <96093472+igorcfer@users.noreply.github.com>
2023-01-06 15:01:42 -0500

    final project

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const btnElement = document.getElementById("btn")
-const emojiNameElement = document.getElementById("emoji-name")
+const btnElement = document.getElementById("btn");
+const emojiNameElement = document.getElementById("emoji-name");
 
-btnElement.addEventListener("click", ()=>{
-    console.log("cliked");
-})
+const emoji = [];
+
+async function getEmoji() {
+  let response = await fetch(
+    "https://emoji-api.com/emojis?access_key=ba4474e948b87c6dad42f7fad49364cf4d37b877"
+  );
+
+  data = await response.json();
+
+  for (let i = 0; i < 1500; i++) {
+    emoji.push({
+      emojiName: data[i].character,
+      emojiCode: data[i].unicodeName,
+    });
+  }
+}
+
+getEmoji();
+
+btnElement.addEventListener("click", () => {
+  const randomNumber = Math.floor(Math.random() * emoji.length);
+  btnElement.innerText = emoji[randomNumber].emojiName;
+  emojiNameElement.innerText = emoji[randomNumber].emojiCode;
+});
